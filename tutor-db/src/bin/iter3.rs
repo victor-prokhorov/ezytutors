8d5a6336a3a98811871ed9f025fb9168de6e5123
Victor Prokhorov <mail@victorprokhorov.com>
2024-09-24 15:08:27 +0200

    iter3

---
+++ tutor-db/src/bin/iter3.rs
@@ -0,0 +1,41 @@
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::PgPool;
+use std::{
+    env, io,
+    sync::{Arc, Mutex},
+};
+
+#[path = "../iter3/handlers.rs"]
+mod handlers;
+#[path = "../iter3/routes.rs"]
+mod routes;
+use routes::*;
+#[path = "../iter3/state.rs"]
+mod state;
+use state::AppState;
+#[path = "../iter3/db_access.rs"]
+mod db_access;
+#[path = "../iter3/models.rs"]
+mod models;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    // for dev environment to easy loadig .env
+    dotenv().ok();
+    let databse_url = env::var("DATABASE_URL").expect("set DATABASE_URL in .env");
+    let db_pool = PgPool::connect(&databse_url).await.unwrap();
+    db_access::get_courses_for_tutor_db(&db_pool, 1).await;
+    let shared_data = web::Data::new(AppState {
+        health_check_response: String::new(),
+        visit_count: Mutex::new(0),
+        db: db_pool,
+    });
+    let app = move || {
+        App::new()
+            .app_data(Arc::clone(&shared_data))
+            .configure(general_routes)
+            .configure(course_routes)
+    };
+    HttpServer::new(app).bind("127.0.0.1:3000")?.run().await
+}
---
+++ tutor-db/src/iter3/db_access.rs
@@ -0,0 +1,39 @@
+use super::models::Course;
+use sqlx::postgres::PgPool;
+
+pub async fn get_courses_for_tutor_db(pool: &PgPool, tutor_id: i32) -> Vec<Course> {
+    let course_rows = sqlx::query!(
+        "SELECT tutor_id, course_id, course_name, posted_time FROM
+       ezy_course_c4 where tutor_id = $1",
+        tutor_id
+    )
+    .fetch_all(pool)
+    .await
+    .unwrap();
+    course_rows
+        .iter()
+        .map(|course_row| Course {
+            course_id: course_row.course_id,
+            tutor_id: course_row.tutor_id,
+            course_name: course_row.course_name.clone(),
+            posted_time: Some(chrono::NaiveDateTime::from(course_row.posted_time.unwrap())),
+        })
+        .collect()
+}
+
+pub async fn get_course_details_db(pool: &PgPool, tutor_id: i32, course_id: i32) -> Course {
+    let course_row = sqlx::query!(
+        "SELECT tutor_id, course_id, course_name, posted_time FROM ezy_course_c4 WHERE tutor_id = $1 AND course_id = $2",
+        tutor_id,
+            course_id ,
+            )
+        .fetch_one(pool)
+            .await
+            .unwrap();
+    Course {
+        course_id: course_row.course_id,
+        tutor_id: course_row.tutor_id,
+        course_name: course_row.course_name.clone(),
+        posted_time: Some(chrono::NaiveDateTime::from(course_row.posted_time.unwrap())),
+    }
+}
